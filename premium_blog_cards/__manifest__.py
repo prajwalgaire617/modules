c8c15c195d8199be2f0d4ed4c61a625487082032
Prajwal Gaire <86487027+prajwalgaire617@users.noreply.github.com>
2026-03-29 14:56:49 +0545

    Update __manifest__.py

--- premium_blog_cards/__manifest__.py
+++ premium_blog_cards/__manifest__.py
@@ -24,8 +24,6 @@ Features:
         'web.assets_frontend': [
             'premium_blog_cards/static/src/scss/premium_blog_cards.scss',
             'premium_blog_cards/static/src/scss/chart_custom.scss',
-            'premium_blog_cards/static/src/js/stability_patches.js',
-            'premium_blog_cards/static/src/js/custom_carousels.js',
         ],
     },
     'installable': True,
